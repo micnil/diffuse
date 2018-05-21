3fbc35eb705aa5dff7ba3e68f78c1eefae8523f1
Michael Nilsson <michaelnilsson90@hotmail.com>
2018-05-21 21:38:57 +0200

    add ActivityBar to view.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import ActivityBar from './containers/ActivityBar';
+import './App.css';
 
 export class App extends Component {
-
 	render() {
 		return (
-			<div>
-                {'Hello World'}
+			<div className={'app'}>
+                <div className={'activity-bar-container'}>
+					<ActivityBar />
+				</div>
 			</div>
 		);
 	}
--- src/renderer/containers/ActivityBar.tsx
+++ src/renderer/containers/ActivityBar.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, CSSProperties } from 'react';
 import { connect } from 'react-redux';
 import { IState } from '../reducers';
 import { Dispatch } from '../types';
-import ListItemIcon from '@material-ui/icons/List';
-import ListIcon from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/List';
+import ListIcon from '@material-ui/icons/List';
 import CompareIcon from '@material-ui/icons/Compare';
 import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -11,8 +11,8 @@ import { createNavigation } from '../actions';
 
 const styles = {
 	listItemIcon: {
-		margin: 'auto',
-	} as React.CSSProperties
+		display: 'inline-flex',
+	} as CSSProperties
 };
 
 interface IActivityBarProps {
